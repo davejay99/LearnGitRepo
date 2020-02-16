a27d23c7521bd0ecc47764dbb467b8b45975c68a
Jay Dave <davejoy9999@gmail.com>
2020-02-16 22:56:37 +0530

    initial commit(abc.c file)

---
+++ abc.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dif(int a,int b)
+{
+	return abs(a-b);
+}
+int main()
+{
+	vector<int> a(3);
+	int d;
+	for(int i=0;i<3;i++)
+	{
+		cin>>a[i];
+	}
+	cin>>d;
+	sort(a.begin(),a.end());
+	int x,y,z;
+	x=abs(a[0]-a[1]);
+	y=abs(a[1]-a[2]);
+	z=abs(a[0]-a[2]);
+	int c=0;
+	while(x<d || y<d || z<d)
+	{
+		if(x<d)
+		{
+			c=c+dif(x,d);
+			a[0]=a[0]-dif(x,d);
+			x=dif(a[0],a[1]);
+			z=dif(a[0],a[2]);
+		}
+		if(y<d)
+		{
+			c=c+dif(y,d);
+			a[2]=a[2]+dif(y,d);
+			y=dif(a[1],a[2]);
+			z=dif(a[0],a[2]);
+		}
+	
+	}
+	cout<<c<<endl;
+	
+}
