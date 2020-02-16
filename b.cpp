44f67114884cadeab5f77a405135c0403be546a2
Jay Dave <davejoy9999@gmail.com>
2020-02-16 22:59:13 +0530

    b.cpp uploaded

---
+++ b.cpp
@@ -0,0 +1,22 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int> a(n);
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	sort(a.begin(),a.end());
+	int c=a[n-2]-a[0],b=a[n-1]-a[1];
+	if(c>b)
+	{
+		cout<<b;
+	}
+	else{
+	cout<<c;}
+	return 0;
+}
